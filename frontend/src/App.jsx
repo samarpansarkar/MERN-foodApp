20e6736532b00fb020b8011d495b360e736f667f
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-06-21 22:24:27 +0530

    Project setup and create project navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar"
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css"
+import { assets } from '../../assets/assets.js';
+import { useState } from "react";
+
+const Navbar = () => {
+    const [menu, setMenu] = useState('home');
+    return (
+        <div className="navbar">
+            <img src={assets.logo} alt="Logo" className="logo" />
+            <ul className="navbar-menu">
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
+                <li onClick={() => setMenu("mobile")} className={menu === "mobile" ? "active" : ""}>mobile-app</li>
+                <li onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>contact us</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="Search" className="search-icon" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>sign in</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
