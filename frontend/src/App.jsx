9ac5d9c084f5a96b1ac2476894ecf34d1257c07c
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-06-21 23:45:44 +0530

    adding tailwind and modify navbar css.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="w-10/12 m-auto pt-4">
       <Navbar />
       <h1>Hello, World!</h1>
     </div>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
-import "./Navbar.css"
 import { assets } from '../../assets/assets.js';
 import { useState } from "react";
 
 const Navbar = () => {
     const [menu, setMenu] = useState('home');
+
     return (
-        <div className="navbar">
-            <img src={assets.logo} alt="Logo" className="logo" />
-            <ul className="navbar-menu">
-                <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
-                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
-                <li onClick={() => setMenu("mobile")} className={menu === "mobile" ? "active" : ""}>mobile-app</li>
-                <li onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>contact us</li>
+        <div className="p-20px flex justify-between items-center  ">
+            <img src={assets.logo} alt="Logo" className="w-36" />
+            <ul className="flex list-none gap-5 text-[#49557e] text-lg cursor-pointer">
+                <li onClick={() => setMenu("home")} className={menu === "home" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>home</li>   {/*  pb-1 border-b-2 border-solid border-orange-600 */}
+                <li onClick={() => setMenu("menu")} className={menu === "menu" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>menu</li>
+                <li onClick={() => setMenu("mobile")} className={menu === "mobile" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>mobile-app</li>
+                <li onClick={() => setMenu("contact")} className={menu === "contact" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>contact us</li>
             </ul>
-            <div className="navbar-right">
-                <img src={assets.search_icon} alt="Search" className="search-icon" />
-                <div className="navbar-search-icon">
+            <div className="flex items-center gap-10">
+                <img src={assets.search_icon} alt="Search" className="relative" />
+                <div className="relative">
                     <img src={assets.basket_icon} alt="" />
-                    <div className="dot"></div>
+                    <div className="absolute min-w-3 min-h-3 bg-orange-600 border-solid rounded-[50%] top-[-8px] right-[-8px]"></div>
                 </div>
-                <button>sign in</button>
+                <button className="text-base px-3 py-2 border-2 border-solid border-orange-300 rounded-2xl  hover:bg-orange-600 hover:text-white">sign in</button>
             </div>
         </div>
     )
