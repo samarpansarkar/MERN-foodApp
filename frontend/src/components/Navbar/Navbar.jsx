cace283e8e48dfddff6b0f41e65c3c3386fdfcc7
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-06-28 01:14:05 +0530

    cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,84 @@
-import { Link } from 'react-router-dom';
-import { assets } from '../../assets/assets.js';
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets.js";
+import { useContext, useState } from "react";
+import { StoreContext } from "../../context/StoreContext.jsx";
 
 const Navbar = ({ showLogin, setShowLogin }) => {
-    const [menu, setMenu] = useState('home');
+  const [menu, setMenu] = useState("home");
 
-    return (
-        <div className="p-20px flex justify-between items-center ">
-            <img src={assets.logo} alt="Logo" className="w-36" />
-            <ul className="flex list-none gap-5 text-[#49557e] text-lg cursor-pointer">
-                <Link to="/" onClick={() => setMenu("home")} className={menu === "home" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>home</Link>   {/*  pb-1 border-b-2 border-solid border-orange-600 */}
-                <a href="#explore-menu" onClick={() => setMenu("menu")} className={menu === "menu" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>menu</a>
-                <a href="#app-download" onClick={() => setMenu("mobile")} className={menu === "mobile" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>mobile app</a>
-                <a href="#footer" onClick={() => setMenu("contact")} className={menu === "contact" ? "pb-1 border-b-2 border-solid border-orange-600" : ""}>contact us</a>
-            </ul>
-            <div className="flex items-center gap-10">
-                <img src={assets.search_icon} alt="Search" className="relative" />
-                <div className="relative">
-                    <img src={assets.basket_icon} alt="" />
-                    <div className="absolute min-w-3 min-h-3 bg-orange-600 border-solid rounded-[50%] top-[-8px] right-[-8px]"></div>
-                </div>
-                <button onClick={() => { showLogin ? setShowLogin(false) : setShowLogin(true) }} className="text-base px-3 py-2 border-2 border-solid border-orange-300 rounded-2xl  hover:bg-orange-600 hover:text-white">sign in</button>
-            </div>
+  const { getTotalCartAmount } = useContext(StoreContext);
+
+  return (
+    <div className='p-20px flex justify-between items-center '>
+      <Link to='/'>
+        <img src={assets.logo} alt='Logo' className='w-36' />
+      </Link>
+      <ul className='flex list-none gap-5 text-[#49557e] text-lg cursor-pointer'>
+        <Link
+          to='/'
+          onClick={() => setMenu("home")}
+          className={
+            menu === "home"
+              ? "pb-1 border-b-2 border-solid border-orange-600"
+              : ""
+          }>
+          home
+        </Link>{" "}
+        {/*  pb-1 border-b-2 border-solid border-orange-600 */}
+        <a
+          href='#explore-menu'
+          onClick={() => setMenu("menu")}
+          className={
+            menu === "menu"
+              ? "pb-1 border-b-2 border-solid border-orange-600"
+              : ""
+          }>
+          menu
+        </a>
+        <a
+          href='#app-download'
+          onClick={() => setMenu("mobile")}
+          className={
+            menu === "mobile"
+              ? "pb-1 border-b-2 border-solid border-orange-600"
+              : ""
+          }>
+          mobile app
+        </a>
+        <a
+          href='#footer'
+          onClick={() => setMenu("contact")}
+          className={
+            menu === "contact"
+              ? "pb-1 border-b-2 border-solid border-orange-600"
+              : ""
+          }>
+          contact us
+        </a>
+      </ul>
+      <div className='flex items-center gap-10'>
+        <img src={assets.search_icon} alt='Search' className='relative' />
+        <div className='relative'>
+          <Link to='/cart'>
+            <img src={assets.basket_icon} alt='' />
+          </Link>
+          <div
+            className={
+              getTotalCartAmount() === 0
+                ? ""
+                : "absolute min-w-3 min-h-3 bg-orange-600 border-solid rounded-[50%] top-[-8px] right-[-8px] animate-bounce"
+            }></div>
         </div>
-    )
-}
+        <button
+          onClick={() => {
+            showLogin ? setShowLogin(false) : setShowLogin(true);
+          }}
+          className='text-base px-3 py-2 border-2 border-solid border-orange-300 rounded-2xl  hover:bg-orange-600 hover:text-white'>
+          sign in
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/Cart/CartPage.jsx
+++ frontend/src/pages/Cart/CartPage.jsx
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 const CartPage = () => {
-    return (
-        <div>
-            <h1>CartPage</h1>
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
+  return (
+    <div className='mt-24'>
+      <div className=''>
+        <div className='grid grid-flow-col grid-cols-5 items-center text-lg justify-between'>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
         </div>
-    )
-}
+        <br />
+        <hr className='border-black' />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index} className='bg-orange-400'>
+                <div
+                  className='grid grid-flow-col grid-cols-6 items-center text-lg  text-center mx-3 justify-between py-2  '
+                  key={index}>
+                  <img src={item.image} alt={item.name} className='w-12' />
+                  <p>{item.name}</p>
+                  <p>$ {item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>$ {item.price * cartItems[item._id]}</p>
+                  <p
+                    onClick={() => {
+                      removeFromCart(item._id);
+                    }}
+                    className='cursor-pointer'>
+                    x
+                  </p>
+                </div>
+                <hr className='border-none h-1 bg-orange-100' />
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className='mt-20 flex justify-between gap-5'>
+        <div className='flex-1 flex flex-col gap-5'>
+          <h2>Cart Total</h2>
+          <div>
+            <div className='flex justify-between p-2'>
+              <p>Subtotal</p>
+              <p>$ {getTotalCartAmount()}</p>
+            </div>
+            <hr className='border-none h-1 bg-orange-100' />
+            <div className='flex justify-between p-2'>
+              <p>Delivery Fee</p>
+              <p>$ {2}</p>
+            </div>
+            <hr className='border-none h-1 bg-orange-100' />
+            <div className='flex justify-between p-2'>
+              <b>Total</b>
+              <b>$ {getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button className='text-base w-fit px-3 py-2 border-2 border-solid bg-orange-300 border-orange-500 rounded-xl  hover:bg-orange-600 hover:text-white'>
+            PROCEED TO CHECKOUT
+          </button>
+        </div>
+        <div className='flex-1'>
+          <div>
+            <p className='text-orange-500'>
+              If you have a promo code, Enter it here
+            </p>
+            <div className='flex gap-4'>
+              <input
+                type='text'
+                placeholder='Promo Code'
+                className='p-2 border-2 border-orange-500 rounded-md text-center'
+              />
+              <button className='text-base px-3 py-2 border-2 border-solid bg-orange-300 border-orange-500 rounded-xl  hover:bg-orange-600 hover:text-white'>
+                APPLY
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CartPage
+export default CartPage;
