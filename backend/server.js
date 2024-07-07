ed6e1dbea2a84fc2537e1486c324d23240756d9f
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-07-07 12:16:15 +0530

    user login and registration.

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
-
+import userRouter from './routes/userRoute.js';
+import "dotenv/config"
 const app = express();
 const PORT = process.env.PORT;
 
@@ -18,6 +19,7 @@ connectDB();
 //!Api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static('uploads'))
+app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
     res.send('API working!!!');
