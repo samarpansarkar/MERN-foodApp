21f7403ab5bcf8e6cee8b2d096217bc710ce1c82
samarpan sarkar <69873435+samarpansarkar@users.noreply.github.com>
2024-07-19 20:56:25 +0530

    user order page (frontend)

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "./MyOrders.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { assets } from './../../assets/assets';
 
 const MyOrders = () => {
   const [data, setData] = useState([]);
@@ -18,7 +19,7 @@ const MyOrders = () => {
     );
 
     setData(response.data.data);
-    console.log(response.data.data, data);
+    console.log(data);
   };
 
   useEffect(() => {
@@ -29,7 +30,27 @@ const MyOrders = () => {
   return (
     <div className='my-orders'>
       <h2>My Orders</h2>
-    </div>
+      <div className="container">
+        {data.map((order, index) => {
+          return (
+            <div key={index} className="my-orders-order">
+              <img src={assets.parcel_icon} alt="" />
+              <p>{order.items.map((item, index) => {
+                if (index === order.items.length - 1) {
+                  return item.name + " X " + item.quantity
+                } else {
+                  return item.name + " X " + item.quantity + ","
+                }
+              })}</p>
+              <p>${order.amount}.00</p>
+              <p>Items: {order.items.length}</p>
+              <p><span>&#x25cf;</span><b>{order.status}</b></p>
+              <button>Track Order</button>
+            </div>
+          )
+        })}
+      </div>
+    </div >
   );
 };
 
